2bccf21dcd4e8d55394cf6330943d41f317db9c2
Francesco Abbate <francesco.bbt@gmail.com>
2024-10-18 20:33:34 +0200

    debug: code to save surfaces as PNG for debugging

---
+++ src/debug-image-save.c
@@ -0,0 +1,92 @@
+
+static int saveSurfaceToPNG(SDL_Surface* surface, const char* filename) {
+    if (IMG_SavePNG(surface, filename) != 0) {
+        // Handle error
+        SDL_Log("Failed to save PNG: %s", SDL_GetError());
+        return -1;
+    }
+    return 0;
+}
+
+void save_texture(SDL_Renderer *ren, SDL_Texture *tex, const char *filename)
+{
+    SDL_Texture *ren_tex;
+    SDL_Surface *surf;
+    int st;
+    int w;
+    int h;
+    int format;
+    void *pixels;
+
+    pixels  = NULL;
+    surf    = NULL;
+    ren_tex = NULL;
+    format  = SDL_PIXELFORMAT_RGBA32;
+
+    /* Get information about texture we want to save */
+    st = SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+    if (st != 0) {
+        SDL_Log("Failed querying texture: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    ren_tex = SDL_CreateTexture(ren, format, SDL_TEXTUREACCESS_TARGET, w, h);
+    if (!ren_tex) {
+        SDL_Log("Failed creating render texture: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    /*
+     * Initialize our canvas, then copy texture to a target whose pixel data we 
+     * can access
+     */
+    st = SDL_SetRenderTarget(ren, ren_tex);
+    if (st != 0) {
+        SDL_Log("Failed setting render target: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(ren);
+
+    st = SDL_RenderCopy(ren, tex, NULL, NULL);
+    if (st != 0) {
+        SDL_Log("Failed copying texture data: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    /* Create buffer to hold texture data and load it */
+    pixels = malloc(w * h * SDL_BYTESPERPIXEL(format));
+    if (!pixels) {
+        SDL_Log("Failed allocating memory\n");
+        goto cleanup;
+    }
+
+    st = SDL_RenderReadPixels(ren, NULL, format, pixels, w * SDL_BYTESPERPIXEL(format));
+    if (st != 0) {
+        SDL_Log("Failed reading pixel data: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    /* Copy pixel data over to surface */
+    surf = SDL_CreateRGBSurfaceWithFormatFrom(pixels, w, h, SDL_BITSPERPIXEL(format), w * SDL_BYTESPERPIXEL(format), format);
+    if (!surf) {
+        SDL_Log("Failed creating new surface: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    /* Save result to an image */
+    st = IMG_SavePNG(surf, filename);
+    if (st != 0) {
+        SDL_Log("Failed saving image: %s\n", SDL_GetError());
+        goto cleanup;
+    }
+
+    SDL_Log("Saved texture as PNG to \"%s\"\n", filename);
+
+cleanup:
+    SDL_FreeSurface(surf);
+    free(pixels);
+    SDL_DestroyTexture(ren_tex);
+}
+
--- src/renwindow.c
+++ src/renwindow.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "renwindow.h"
 
+#include <SDL_image.h>
+
 /* Query surface size and returns the scale factor. */
 int renwin_get_size(RenWindow *ren, int *w_pixels, int *h_pixels) {
   int w_points, h_points;
@@ -24,10 +26,19 @@ void renwin_resize_window(RenWindow *ren) {
   ren->scale = renwin_get_size(ren, &ren->w_pixels, &ren->h_pixels);
 }
 
+#include "debug-image-save.c"
+
 void renwin_render_surface(RenWindow *ren, RenSurface *rs, int x, int y) {
   /* Width and height of the surface, in pixels. */
   const int w = rs->surface->w, h = rs->surface->h;
   fprintf(stderr, "DEBUG: render surface (%4d, %4d) to position (%4d, %4d)\n", w, h, x, y);
+  if (0 && w > 0 && h > 0) {
+    char buf[1024];
+    sprintf(buf, "surface-%d-%d-%d-%d.png", x, y, w, h);
+    saveSurfaceToPNG(rs->surface, buf);
+    sprintf(buf, "texture-%d-%d-%d-%d.png", x, y, w, h);
+    save_texture(ren->renderer, rs->texture, buf);
+  }
   const SDL_Rect dst = { x, y, w, h };
   SDL_RenderCopy(ren->renderer, rs->texture, NULL, &dst);
 }
